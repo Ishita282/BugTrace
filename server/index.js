9f102bbe89291f8a1d40e82738fac328e613434c
Ishita282 <littleminiishita@gmail.com>
2026-04-18 21:33:52 +0530

    cors link updated

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "https://bug-trace-beta.vercel.app/"
 }));
 app.use(express.json());
 app.use(errorHandler);
